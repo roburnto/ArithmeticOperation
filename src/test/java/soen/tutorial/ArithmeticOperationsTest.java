a8411ee2a25fc106122c044091dc4ed53b64922e
roburnto <58539522+roburnto@users.noreply.github.com>
2025-03-26 10:39:46 -0400

    ensures it fails

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -3,6 +3,7 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ArithmeticOperationsTest {
 
@@ -17,15 +18,17 @@ public class ArithmeticOperationsTest {
 		assertEquals(expected, actual);
 
 	}
-    // @Test
-    // public void testSub()
-    // {
+    @Test
+    public void testSub()
+    {
 
-    //     ArithmeticOperations operations = new ArithmeticOperations();
-    //     Integer actual = operations.addOrSub(6, 2);
-    //     Integer expected = 4;
-    //     assertEquals(expected, actual);
+        ArithmeticOperations operations = new ArithmeticOperations();
+        Integer actual = operations.addOrSub(6, 2);
+        Integer expected = 4;
+        fail();
+        assertEquals(expected, actual);
 
-    // }
+
+    }
 
 }
